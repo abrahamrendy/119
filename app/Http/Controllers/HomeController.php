86d7731a2fd9e594d0b4d3f77f1e17571cc60dca
abrahamrendy <aeroponse@gmail.com>
2022-04-12 03:53:06 +0700

    set off limiter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,9 +33,9 @@ class HomeController extends Controller
     {
         $count = DB::table('registrant')->where('type',0)->count(); //COUNT JEMAAT
         $limit = false;
-        if ($count >= 100) {
-            $limit = true;
-        }
+        // if ($count >= 100) {
+        //     $limit = true;
+        // }
         return view('home',['limit'=>$limit]);
     }
 
